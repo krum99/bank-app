8b5196b195ea4097ad942ba2c7a44344dc581309
krum99 <79230210+krum99@users.noreply.github.com>
2025-05-18 17:51:42 +0300

    (fix): bug - let fastAPI allow requests from port 5173

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,18 @@ from fastapi import FastAPI
 from database import engine
 from models.base import Base
 from auth_routes import router as auth_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # Create tables if they don't exist
 Base.metadata.create_all(bind=engine)
